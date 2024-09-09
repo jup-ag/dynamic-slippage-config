a3e3eacaf562d925795bf0971bf0ba2292b2fe2c
Arrowana <Arrowana@users.noreply.github.com>
2024-09-09 15:41:18 +1000

    feat: Amplification ratio

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
+use range::Range;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use serde::Deserialize;
 use serde_json::{self, error::Result};
 use solana_program::pubkey::Pubkey;
 
 mod field_as_string;
+mod range;
 
-#[derive(Copy, Clone, Deserialize, PartialEq, Debug, Default)]
-pub struct Range {
-    pub min: u16,
-    pub max: u16,
-}
+const DYNAMIC_SLIPPAGE_CONFIG_JSON: &str = include_str!("../dynamic_slippage_config.json");
+
 pub struct Category {
     pub name: String,
     pub range: Range,
+    /// This range applies when the 2 traded mints belong to that category
     pub pair_range: Option<Range>,
     pub mints: Vec<Pubkey>,
 }
@@ -29,31 +31,44 @@ struct CategoryOriginal {
 #[serde(rename_all = "camelCase")]
 pub struct DeserializablePubkey(#[serde(with = "field_as_string")] pub Pubkey);
 
+fn default_amplification_ratio() -> Decimal {
+    dec!(1.5)
+}
+
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct DefaultSlippage {
+
+pub struct DefaultAutoSlippage {
+    #[serde(default = "default_amplification_ratio")]
+    pub amplification_ratio: Decimal,
     pub range: Range,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct DynamicSlippageCategories {
+struct DynamicSlippageConfigOriginal {
     categories: Vec<CategoryOriginal>,
-    default: DefaultSlippage,
+    default: DefaultAutoSlippage,
 }
 
-const DYNAMIC_SLIPPAGE_CONFIG_JSON: &str = include_str!("../dynamic_slippage_config.json");
+pub struct DynamicSlippageConfig {
+    pub categories: Vec<Category>,
+    pub default: DefaultAutoSlippage,
+}
 
-pub fn deserialize_dynamic_slippage_config() -> Result<(DefaultSlippage, Vec<Category>)> {
-    let DynamicSlippageCategories {
+fn deserialize_dynamic_slippage_config(
+    dynamic_slippage_config: &str,
+) -> Result<DynamicSlippageConfig> {
+    let DynamicSlippageConfigOriginal {
         categories,
         default,
-    } = serde_json::from_str::<DynamicSlippageCategories>(DYNAMIC_SLIPPAGE_CONFIG_JSON)?;
+    } = serde_json::from_str::<DynamicSlippageConfigOriginal>(dynamic_slippage_config)?;
     let categories = categories
         .into_iter()
         .map(
             |CategoryOriginal {
                  name,
+
                  range,
                  pair_range,
                  mints,
@@ -66,7 +81,15 @@ pub fn deserialize_dynamic_slippage_config() -> Result<(DefaultSlippage, Vec<Cat
         )
         .collect();
 
-    Ok((default, categories))
+    Ok(DynamicSlippageConfig {
+        categories,
+        default,
+    })
+}
+
+#[allow(dead_code)]
+fn deserialize_embedded_dynamic_slippage_config() -> Result<DynamicSlippageConfig> {
+    deserialize_dynamic_slippage_config(DYNAMIC_SLIPPAGE_CONFIG_JSON)
 }
 
 #[cfg(test)]
@@ -77,7 +100,8 @@ mod tests {
 
     #[test]
     fn test_deserialize_dynamic_slippage_config() {
-        let (_, categories) = deserialize_dynamic_slippage_config().unwrap();
+        let DynamicSlippageConfig { categories, .. } =
+            deserialize_embedded_dynamic_slippage_config().unwrap();
 
         assert_eq!(
             categories.iter().map(|c| &c.name).collect::<Vec<_>>(),
