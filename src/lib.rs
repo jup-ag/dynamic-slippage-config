c53e30d72750abbd42c8b9e214a5b340b9180e20
Desmond <2226903+boxme@users.noreply.github.com>
2025-02-11 23:02:03 +0800

    Remove old slippage config files (#36)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use range::Range;
-use rust_decimal::Decimal;
 use serde::Deserialize;
 use serde_json::{self, error::Result};
 use solana_program::pubkey::Pubkey;
@@ -7,82 +6,44 @@ use solana_program::pubkey::Pubkey;
 mod field_as_string;
 mod range;
 
-const DYNAMIC_SLIPPAGE_CONFIG_JSON: &str = include_str!("../dynamic_slippage_config.json");
+const TOKEN_CATEGORIES_JSON: &str = include_str!("../token_categories.json");
+const SLIPPAGE_CONFIG_JSON: &str = include_str!("../slippage_config.json");
 
+#[derive(Deserialize)]
 pub struct Category {
     pub name: String,
-    pub range: Range,
-    /// This range applies when the 2 traded mints belong to that category
-    pub pair_range: Option<Range>,
-    pub mints: Vec<Pubkey>,
+    pub mints: Vec<DeserializablePubkey>,
 }
 
 #[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct CategoryOriginal {
-    name: String,
-    range: Range,
-    pair_range: Option<Range>,
-    mints: Vec<DeserializablePubkey>,
+pub struct TokenCategories {
+    pub categories: Vec<Category>,
+    pub excluded_from_intermediate_hop_mints: Vec<DeserializablePubkey>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, PartialEq, Eq, Hash, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct DeserializablePubkey(#[serde(with = "field_as_string")] pub Pubkey);
 
 #[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct DynamicSlippageDefault {
-    pub amplification_ratio: Decimal,
-    pub range: Range,
+pub struct SlippageConfig {
+    pub slippage_base_tolerance: Vec<SlippageRange>,
 }
 
 #[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct DynamicSlippageConfigOriginal {
-    categories: Vec<CategoryOriginal>,
-    default: DynamicSlippageDefault,
-}
-
-pub struct DynamicSlippageConfig {
-    pub categories: Vec<Category>,
-    pub default: DynamicSlippageDefault,
-}
-
-fn deserialize_dynamic_slippage_config(
-    dynamic_slippage_config: &str,
-) -> Result<DynamicSlippageConfig> {
-    let DynamicSlippageConfigOriginal {
-        categories,
-        default,
-    } = serde_json::from_str::<DynamicSlippageConfigOriginal>(dynamic_slippage_config)?;
-    let categories = categories
-        .into_iter()
-        .map(
-            |CategoryOriginal {
-                 name,
-
-                 range,
-                 pair_range,
-                 mints,
-             }| Category {
-                name,
-                range,
-                pair_range,
-                mints: mints.into_iter().map(|m| m.0).collect(),
-            },
-        )
-        .collect();
-
-    Ok(DynamicSlippageConfig {
-        categories,
-        default,
-    })
+pub struct SlippageRange {
+    pub name: String,
+    pub range: Range,
 }
 
 #[allow(dead_code)]
-fn deserialize_embedded_dynamic_slippage_config() -> Result<DynamicSlippageConfig> {
-    deserialize_dynamic_slippage_config(DYNAMIC_SLIPPAGE_CONFIG_JSON)
+fn deserialize_token_categories() -> Result<TokenCategories> {
+    serde_json::from_str::<TokenCategories>(TOKEN_CATEGORIES_JSON)
+}
+
+#[allow(dead_code)]
+fn deserialize_slippage_config() -> Result<SlippageConfig> {
+    serde_json::from_str::<SlippageConfig>(SLIPPAGE_CONFIG_JSON)
 }
 
 #[cfg(test)]
@@ -92,28 +53,43 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_deserialize_dynamic_slippage_config() {
-        let DynamicSlippageConfig { categories, .. } =
-            deserialize_embedded_dynamic_slippage_config().unwrap();
+    fn test_deserialize_token_categories_config() {
+        let TokenCategories { categories, .. } = deserialize_token_categories().unwrap();
 
         assert_eq!(
             categories.iter().map(|c| &c.name).collect::<Vec<_>>(),
-            vec!["stable", "lst", "bluechip", "verified"]
+            vec!["solana", "stable", "lst", "bluechip"]
         );
-        assert!(categories
-            .iter()
-            .find(|c| c.name == "lst")
-            .unwrap()
-            .pair_range
-            .is_some());
 
         // Ensure no duplicated mints, otherwise the config behaviour will be confusing
         let duplicates = categories
             .iter()
             .flat_map(|c| &c.mints)
             .duplicates()
-            .collect::<Vec<&Pubkey>>();
+            .collect::<Vec<&DeserializablePubkey>>();
 
-        assert_eq!(duplicates, Vec::<&Pubkey>::new());
+        assert_eq!(duplicates, Vec::<&DeserializablePubkey>::new());
+    }
+
+    #[test]
+    fn test_deserialize_slippage_config() {
+        let slippage_config = deserialize_slippage_config().unwrap();
+
+        assert_eq!(
+            slippage_config
+                .slippage_base_tolerance
+                .iter()
+                .map(|c| &c.name)
+                .collect::<Vec<_>>(),
+            vec![
+                "solana",
+                "stable",
+                "lst",
+                "bluechip",
+                "default",
+                "pump_new_graduated",
+                "degen"
+            ]
+        );
     }
 }
